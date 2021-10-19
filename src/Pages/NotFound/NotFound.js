e0d0734733299cf02e1ba6c8ca8616719fd8538d
Zihad550 <hussainmuzahid83@gmail.com>
2021-10-19 23:54:03 +0600

    404 page added

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <div></div>;
+  return (
+    <div className="vh-100 vw-100 d-flex align-items-center justify-content-center">
+      <div>
+        <h1 className="text-warning fw-bold fs-1">
+          The Page you are looking for is not available
+        </h1>
+        <Link to="/home" className="fs-5">
+          Return to Home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
